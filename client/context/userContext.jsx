f4206aa332d588a749a05d0315b3f64c05b88fbf
moadhamousti <moadhamousti@gmail.com>
2024-08-18 19:36:14 +0100

    Update

--- client/context/userContext.jsx
+++ client/context/userContext.jsx
@@ -43,7 +43,7 @@ export function UserContextProvider({ children }) {
     useEffect(() => {
         const fetchUser = async () => {
             try {
-                const { data } = await axios.get('/api/auth/profile', { withCredentials: true });
+                const { data } = await axios.get('https://learning-cm37.onrender.com/api/auth/profile', { withCredentials: true });
                 setUser(data);
             } catch (error) {
                 console.error('Error fetching user profile:', error.response ? error.response.data : error.message);
@@ -57,7 +57,7 @@ export function UserContextProvider({ children }) {
 
     const login = async (email, password) => {
         try {
-            const { data } = await axios.post('/api/auth/login', { email, password }, { withCredentials: true });
+            const { data } = await axios.post('https://learning-cm37.onrender.com/api/auth/login', { email, password }, { withCredentials: true });
             setUser(data);
             navigate('/'); // Redirect to home after successful login
         } catch (error) {
@@ -67,7 +67,7 @@ export function UserContextProvider({ children }) {
 
     const logout = async () => {
         try {
-            await axios.post('/api/auth/logout', {}, { withCredentials: true });
+            await axios.post('https://learning-cm37.onrender.com/api/auth/logout', {}, { withCredentials: true });
             setUser(null);
             navigate('/login');
         } catch (error) {
